870b2b2b00595fe1d0362c0d0fa2a59978c7c4de
lasteeh <lastdavid.lantican@gmail.com>
2023-01-09 16:12:49 +0800

    react router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,51 @@
 import React from "react";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Services from "./pages/Services";
+import Portfolio from "./pages/Portfolio";
+import Team from "./pages/Team";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
+import { NavLink, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <div className="page-content">
-        <Home />
-        <About />
-      </div>
-    </div>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/services">Services</NavLink>
+          </li>
+          <li>
+            <NavLink to="/portfolio">Portfolio</NavLink>
+          </li>
+          <li>
+            <NavLink to="/team">Team</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+          <li>
+            <a href="https://now.nowsoftwaresolutions.com/">Clients</a>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
